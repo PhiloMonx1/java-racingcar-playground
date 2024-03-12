723b273651a422e9a6b18847398b33923bd7c113
LEE TAE MIN <philomonx1@gmail.com>
2024-03-12 20:31:48 +0900

    Feat(문자열_덧셈_계산기) : 유저 입력 값 int[]로 변환하는 래퍼 클래스 구현 *add : `UserOperand` 래퍼 클래스 추가 *test : `UserOperand` 래퍼 클래스 생성자 테스트 케이스 작성 (기능 요구사항에 명시된 테스트 검증) *docs : 해결된 TodoList 완료 표시

---
+++ src/main/java/step_01/UserOperand.java
@@ -0,0 +1,54 @@
+package step_01;
+
+public class UserOperand {
+	private final String separator;
+	private final String regex;
+	private final int[] numbers;
+	public UserOperand(UserInput userInput) {
+		this.separator = extractSeparator(userInput.getValue());
+		this.regex = createRegex();
+		this.numbers = extractNumbers(userInput.getValue());
+	}
+
+	public String getSeparator() {
+		return separator;
+	}
+
+	public int[] getNumbers() {
+		return numbers;
+	}
+
+	private String extractSeparator(String input) {
+		int delimiterStartIndex = input.indexOf("//") + 2;
+		int delimiterEndIndex = input.indexOf("\\n");
+		if(delimiterEndIndex > -1){
+			return input.substring(delimiterStartIndex, delimiterEndIndex);
+		}
+		return ",";
+	}
+
+	private int[] extractNumbers(String input) {
+		String[] split = replaceInput(input).split(separator + "|,|:");
+		int[] numbers = new int[split.length];
+		for (int i = 0; i < split.length; i++) {
+			numbers[i] = Integer.parseInt((split[i]));
+		}
+		return numbers;
+	}
+
+	private String createRegex() {
+		String regex = "[^0-9,:";
+		if(!separator.isEmpty()){
+			regex += separator;
+		}
+		regex += "]";
+		return regex;
+	}
+
+	private String replaceInput(String input) {
+		input = input.replaceAll(regex, "");
+		input = input.replaceAll("^[^0-9]+", "");
+		input = input.replaceAll("[^0-9]+$", "");
+		return input;
+	}
+}
