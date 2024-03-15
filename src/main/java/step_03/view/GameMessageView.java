f8e70fba5e17729544c545fc4199e0975c86a0a1
LEE TAE MIN <philomonx1@gmail.com>
2024-03-15 11:20:40 +0900

    Refactor(NEW-자동차경주) : `GameMessageView` 내부 메서드 리펙토링 (List<Car> 대신 RacingCars 사용하도록 개선) * add : printCurrentPointOfCars() 메서드 추가

--- src/main/java/step_03/view/GameMessageView.java
+++ src/main/java/step_03/view/GameMessageView.java
@@ -1,19 +1,25 @@
 package step_03.view;
 
-import java.util.List;
 import step_03.domain.Car;
+import step_03.domain.RacingCars;
 
 public class GameMessageView extends MessageView {
 
-	public void printWinners(List<Car> winnersName) {
-		String[] winners = new String[winnersName.size()];
-		for (int i = 0; i < winnersName.size(); i++) {
-			winners[i] = extractCarName(winnersName.get(i));
+	public void printWinners(RacingCars winners) {
+		String[] winnersNames = new String[winners.getRacingCars().size()];
+		for (int i = 0; i < winners.getRacingCars().size(); i++) {
+			winnersNames[i] = extractCarName(winners.getRacingCars().get(i));
 		}
-		super.printWinners(winners);
+		super.printWinners(winnersNames);
 	}
 
-	public void printCurrentPointOfCar(Car car) {
+	public void printCurrentPointOfCars(RacingCars cars) {
+		for (Car car : cars.getRacingCars()) {
+			printCurrentPointOfCar(car);
+		}
+	}
+
+	private void printCurrentPointOfCar(Car car) {
 		super.printCurrentPointOfCar(extractCarName(car), extractCarPoint(car));
 	}
 
